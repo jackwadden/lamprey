652f23fe26641bda73e02737dd621a2fa8eb6a20
jackwadden <jackwadden@gmail.com>
2021-02-05 15:40:01 -0500

    Identified subprocess calls as source of slowdown. Refactored sub-concatemer alignment and pileup into single script (process_candidates.sh) and make a single call. Also removed consensus calling script and capability for now. Will add back in eventually.

---
+++ scripts/process_candidates.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+FASTA=${1}
+SAM_FN=${2}
+REF=${3}
+
+# align each extracted concatemer piece
+minimap2 \
+    -a \
+    -xmap-ont \
+    --eqx \
+    -t 1 \
+    -w 1 \
+    -n 1 \
+    -N 5 \
+    --secondary=no \
+    -m 0 \
+    -p 0.6 \
+    -s 30 \
+    -o ${SAM_FN} \
+    ${REF} \
+    ${FASTA}
+
+KEY=${SAM_FN%_all.sam}
+
+# filter, sort, and generate a pileup of the subs; piping for efficiency
+samtools view -Sb -h -F 0x900 ${SAM_FN} | \
+samtools sort | \
+samtools mpileup \
+         -Q 0 \
+         -f ${REF} \
+	 - \
+         > ${KEY}.pileup
