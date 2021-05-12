8d7925f106b1e24f5c2c88c338d6c60cbb596964
Jack Wadden <jackwadden@gmail.com>
2021-05-12 13:47:29 -0400

    Adding proper paths to example run scripts.

--- scripts/run_h3.1.sh
+++ scripts/run_h3.1.sh
@@ -9,7 +9,10 @@ fi
 REF=${1}
 FASTQ=${2}
 
-python3 ../lamprey.py \
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+
+python3 ${dir_path}/../lamprey.py \
 	--debug_print \
 	--time_sort \
 	--save_target_reads \
@@ -18,11 +21,11 @@ python3 ../lamprey.py \
 	--print_summary_stats \
 	--num_threads 1 \
 	--threshold 0.75 \
-	--ref_vcf_fn ../data/H31/H3.1k27m_ref.vcf \
-	--target_vcf_fn ../data/H31/H3.1k27m_target.vcf \
-	../data/references/HIST1H3B.fa \
+	--ref_vcf_fn ${dir_path}/../data/H31/H3.1k27m_ref.vcf \
+	--target_vcf_fn ${dir_path}/../data/H31/H3.1k27m_target.vcf \
+	${dir_path}/../data/references/HIST1H3B.fa \
 	${REF} \
-	~/lamprey/data/H31/H3.1_set1.primers \
+	${dir_path}/../data/H31/H3.1_set1.primers \
 	${FASTQ}
 
 
--- scripts/run_h3.3.sh
+++ scripts/run_h3.3.sh
@@ -9,8 +9,10 @@ fi
 REF=${1}
 FASTQ=${2}
 
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
 
-python3 ../lamprey.py \
+python3 ${dir_path}/../lamprey.py \
 	--debug_print \
 	--time_sort \
 	--save_target_reads \
@@ -19,11 +21,11 @@ python3 ../lamprey.py \
 	--print_summary_stats \
 	--num_threads 1 \
 	--threshold 0.75 \
-	--ref_vcf_fn ../data/H33/H3.3k27m_ref.vcf \
-	--target_vcf_fn ../data/H33/H3.3k27m_target.vcf \
-	../data/references/H3F3A.fa \
+	--ref_vcf_fn ${dir_path}/../data/H33/H3.3k27m_ref.vcf \
+	--target_vcf_fn ${dir_path}/../data/H33/H3.3k27m_target.vcf \
+	${dir_path}/../data/references/H3F3A.fa \
 	${REF} \
-	~/lamprey/data/H33/H3.3_set1.primers \
+	${dir_path}/../data/H33/H3.3_set1.primers \
 	${FASTQ}
 
 
